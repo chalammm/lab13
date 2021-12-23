d5a3caa2b5f1599a50a1146c956ff784332fc4b6
chalammm <tipanan_ji@cmu.ac.th>
2021-12-23 23:32:07 +0700

    completed_lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,5 +1,16 @@
-#include<iostream>
-using namespace std;
+#include<iostream> 
+#include <ctime>
+
+using namespace std; 
+
+int fibonacci(int x){
+    if(x==0)return 0;
+    else if(x==1)return 1;
+    else if(x>1){
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
+    return 0;
+}
 
 int main(){
     int x;
