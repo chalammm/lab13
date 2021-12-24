26b85008f5e4236791b244fc63bb0dbc92202e88
chalammm <tipanan_ji@cmu.ac.th>
2021-12-24 15:51:34 +0700

    completedlab13_4.cpp

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,31 +2,43 @@
 
 using namespace std;
 
+template<typename T>
+T mySwap(T & , T &);
+
+
+
 int main(){
-	int x, y;
-	string a, b;
-	char p, q;
-	
-	cin >> x >> y >> a >> b >> p >> q;
-	
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	mySwap(a,b);
-	cout << "After swapping:\n";
-	cout << "a = " << a << ", b = " << b << "\n";
-	
-	cout << "Before swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
-	mySwap(p,q);
-	cout << "After swapping:\n";
-	cout << "p = " << p << ", q = " << q << "\n";
+    int x, y;
+    string a, b;
+    char p, q;
 
-	return 0;
+    cin >> x >> y >> a >> b >> p >> q;
+
+    cout << "Before swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+    mySwap(x,y);
+    cout << "After swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+
+    cout << "Before swapping:\n";
+    cout << "a = " << a << ", b = " << b << "\n";
+    mySwap(a,b);
+    cout << "After swapping:\n";
+    cout << "a = " << a << ", b = " << b << "\n";
+
+    cout << "Before swapping:\n";
+    cout << "p = " << p << ", q = " << q << "\n";
+    mySwap(p,q);
+    cout << "After swapping:\n";
+    cout << "p = " << p << ", q = " << q << "\n";
+
+    return 0;
 }
-
+	template <typename T>
+	T mySwap(T &x,T &y)
+{
+    T s = y;
+    y = x;
+    x = s;
+    return x;
+}
