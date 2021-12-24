ee3e9148c41198ddd3860dca731446c6dfa300da
chalammm <tipanan_ji@cmu.ac.th>
2021-12-24 15:48:06 +0700

    completedlab13_3.cpp

--- lab13_3.cpp
+++ lab13_3.cpp
@@ -5,13 +5,22 @@ using namespace std;
 void mySwap(int &,int &); 
 
 int main(){
-	int x,y;
-	cin >> x >> y;
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap (x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	return 0;
+    int x,y;
+    cin >> x >> y;
+    cout << "Before swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+    mySwap (x,y);
+    cout << "After swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+
+    return 0;
+}
+
+void mySwap(int &x, int &y){
+    int a;
+    int b;
+    a = x;
+    b = y;
+    y = a;
+    x = b;
 }
